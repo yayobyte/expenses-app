d50b24daf04f607179765a61936f1716a12f4650
yayobyte <yayobyte@gmail.com>
2023-01-03 19:39:25 +0400

    uuid

--- src/data.ts
+++ src/data.ts
@@ -1,34 +1,59 @@
+import uuid from 'react-uuid'
 import {Expense} from "./constants";
 
 export const DUMMY_EXPENSES: Array<Expense> = [
     {
-        id: 'e1',
+        id: uuid(),
         description: 'Pair of shoes',
         amount: 59.99,
         date: new Date('2022-12-19')
     },
     {
-        id: 'e2',
+        id: uuid(),
         description: 'Gloves',
         amount: 69.99,
         date: new Date('2023-01-22')
     },
     {
-        id: 'e3',
+        id: uuid(),
         description: 'Boots',
         amount: 109.99,
         date: new Date('2022-12-24')
     },
     {
-        id: 'e4',
+        id: uuid(),
         description: 'Shirt',
         amount: 29.99,
         date: new Date('2022-11-11')
     },
     {
-        id: 'e5',
+        id: uuid(),
         description: 'Book',
         amount: 29.99,
+        date: new Date('2022-10-05')
+    },
+    {
+        id: uuid(),
+        description: 'Membership',
+        amount: 19.99,
+        date: new Date('2022-10-05')
+    },
+    {
+        id: uuid(),
+        description: 'Netflix',
+        amount: 49.99,
+        date: new Date('2022-10-05')
+    },
+    {
+        id: uuid(),
+        description: 'Groceries',
+        amount: 99.99,
+        date: new Date('2022-10-05')
+    },
+    {
+        id: uuid(),
+        description: 'Data Cable',
+        amount: 9.99,
         date: new Date('2022-10-05')
     },
 ]
