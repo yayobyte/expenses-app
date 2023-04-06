69279674cb1a9a987bdfd61d43325e19febaaa75
yayobsf <cristian.g@bsfdv.com>
2023-04-06 15:16:33 +0400

    created service to translate

--- translationService/app.js
+++ translationService/app.js
@@ -1,35 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const en = require('./en.json')
+const ar = require('./ar.json')
 
 const app = express();
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 
-const translations = {
-	en: {
-		hello: 'Hello',
-		goodbye: 'Goodbye',
-	},
-	ar: {
-		hello: 'مرحبا',
-		goodbye: 'وداعا',
-	},
-};
-
 app.get('/', async (req, res) => {
     res.send('it works')
 })
 
-app.post('/translate', async (req, res) => {
-	try {
-        const { text, to } = req.body;
-	    const result = translations[to][text];
-        res.send(result);
-    } catch (e) {
-        res.send(e)
-    }
-});
+app.get('/strings', async (req, res) => {
+    const strings = { en, ar }
+    res.send(strings)
+})
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
