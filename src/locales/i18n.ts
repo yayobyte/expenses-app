5d2f2fe8dec8cfd1f5ae72513bd94b827b89b0e7
shukla.bhanvi@gmail.com <bhanvi.s@bsfdv.com>
2023-05-04 17:43:53 +0400

    fix(i18next): fixed cors translation file serve

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,20 +1,20 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import I18NextHttpBackend, { HttpBackendOptions,  } from "i18next-http-backend";
-import { getLocales } from 'expo-localization';
-import en from './en/translation.json'
-import ar from './ar/translation.json'
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import I18NextHttpBackend, { HttpBackendOptions } from "i18next-http-backend";
+import { getLocales } from "expo-localization";
+import en from "./en/translation.json";
+import ar from "./ar/translation.json";
 
 const deviceLanguage = getLocales()[0].languageCode;
 
 const resources = {
-	en: {
-		translation: en,
-	},
-	ar: {
-		translation: ar,
-	}
-}
+  en: {
+    translation: en,
+  },
+  ar: {
+    translation: ar,
+  },
+};
 
 // i18n
 // 	.use(I18NextHttpBackend)
@@ -26,41 +26,60 @@ const resources = {
 // 		resources,
 // 	})
 
-const httpBackendOptions: HttpBackendOptions = {
-	requestOptions: {
-		mode: 'cors',
-		credentials: 'same-origin',
-		cache: 'default',
-		headers: {
-			'Accept': 'application/json',
-			host: 'localhost:3000'
-		}
-	}
-}
+const TRANSLATION_SERVICE = "http://192.168.70.2:3000";
+const requestOptions: HttpBackendOptions["requestOptions"] = {
+  mode: "cors",
+  credentials: "same-origin",
+  cache: "default",
+  headers: {
+    Accept: "application/json, text/html",
+    host: TRANSLATION_SERVICE,
+  },
+};
+// const loadTranslations = (options: HttpBackendOptions) => {
+//   return fetch(`${TRANSLATION_SERVICE}/strings`, { ...options.requestOptions })
+//     .then((res) => res.json())
+//     .then((res) => {
+//       console.log({ res });
+//       return res;
+//     });
+// };
 
-const TRANSLATION_SERVICE = 'http://localhost:3000'
+i18n.use(I18NextHttpBackend).init<HttpBackendOptions>(
+  {
+    debug: true,
+    fallbackLng: "ar",
+    lng: deviceLanguage,
+    load: "languageOnly",
+    partialBundledLanguages: true,
+    react: {
+      useSuspense: true,
+    },
+    backend: {
+      requestOptions,
+      loadPath: `${TRANSLATION_SERVICE}/locales/{{lng}}/{{ns}}.json`,
+      //   request: (options, url, payload, callback) => {
+      //     try {
+      //       loadTranslations(options).then((result) => {
+      //         console.log({ result });
+      //         callback(null, {
+      //           data: result,
+      //           status: 200,
+      //         });
+      //       });
+      //     } catch (e) {
+      //       console.error(e);
+      //       callback(null, {
+      //         data: {},
+      //         status: 500,
+      //       });
+      //     }
+      //   },
+    },
+  },
+  (err, t) => {
+    if (err) return console.error(err);
+  }
+);
 
-const fetchTranslation = (options: HttpBackendOptions) =>
-	fetch(`${TRANSLATION_SERVICE}/strings`, {
-		...options.requestOptions,
-	})
-		// .then((res) => {
-		// 	return res.json()
-		// })
-		// .then ((res) => {
-		// 	console.log(res)
-		// })
-
-
-
-
-i18n
-	.use(I18NextHttpBackend)
-	.init<HttpBackendOptions>({
-		backend: {
-			...httpBackendOptions,
-			request: fetchTranslation,
-		}
-	})
-
-export default i18n
+export default i18n;
