f2c81f99a266abd4f57f0e25d64be14ae2544207
yayobsf <cristian.g@bsfdv.com>
2023-05-04 19:35:48 +0400

    remove unused functionalities

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { ExpensesContextProvider } from "./src/store/expenses.context";
-import { Text } from 'react-native'
 import React, { Suspense } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './src/locales/i18n';
 import { MainNavigator } from "./src/navigation/mainNavigator";
-
-
+import { LoadingOverlay } from "./src/components/UI/LoadingOverlay";
 
 export default function App() {
 	return (
-		<Suspense fallback={<Text>Loading...</Text>}>
+		<Suspense fallback={<LoadingOverlay />}>
 			<I18nextProvider i18n={i18n}>
 				<>
 					<StatusBar style="dark"/>
--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,32 +1,10 @@
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
 import I18NextHttpBackend, { HttpBackendOptions } from "i18next-http-backend";
 import { getLocales } from "expo-localization";
-import en from "./en/translation.json";
-import ar from "./ar/translation.json";
 
 const deviceLanguage = getLocales()[0].languageCode;
 
-const resources = {
-  en: {
-    translation: en,
-  },
-  ar: {
-    translation: ar,
-  },
-};
-
-// i18n
-// 	.use(I18NextHttpBackend)
-// 	.use(initReactI18next)
-// 	.init({
-// 		debug: true,
-// 		fallbackLng: 'ar',
-// 		lng: deviceLanguage,
-// 		resources,
-// 	})
-
-const TRANSLATION_SERVICE = "http://192.168.70.2:3000";
+const TRANSLATION_SERVICE = "http://localhost:3000";
 const requestOptions: HttpBackendOptions["requestOptions"] = {
   mode: "cors",
   credentials: "same-origin",
@@ -36,14 +14,6 @@ const requestOptions: HttpBackendOptions["requestOptions"] = {
     host: TRANSLATION_SERVICE,
   },
 };
-// const loadTranslations = (options: HttpBackendOptions) => {
-//   return fetch(`${TRANSLATION_SERVICE}/strings`, { ...options.requestOptions })
-//     .then((res) => res.json())
-//     .then((res) => {
-//       console.log({ res });
-//       return res;
-//     });
-// };
 
 i18n.use(I18NextHttpBackend).init<HttpBackendOptions>(
   {
@@ -58,23 +28,6 @@ i18n.use(I18NextHttpBackend).init<HttpBackendOptions>(
     backend: {
       requestOptions,
       loadPath: `${TRANSLATION_SERVICE}/locales/{{lng}}/{{ns}}.json`,
-      //   request: (options, url, payload, callback) => {
-      //     try {
-      //       loadTranslations(options).then((result) => {
-      //         console.log({ result });
-      //         callback(null, {
-      //           data: result,
-      //           status: 200,
-      //         });
-      //       });
-      //     } catch (e) {
-      //       console.error(e);
-      //       callback(null, {
-      //         data: {},
-      //         status: 500,
-      //       });
-      //     }
-      //   },
     },
   },
   (err, t) => {
